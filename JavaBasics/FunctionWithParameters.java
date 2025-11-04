5d39032e6f8da8d4a5a20634792f6e43f280ee9a
Jayesh Shivlani <jayeshshivlani23@outlook.com>
2025-11-04 20:31:36 +0530

    Functions & Methods Part-1

---
+++ JavaBasics/FunctionWithParameters.java
@@ -0,0 +1,28 @@
+// Program to show function with parameters
+/* syntax -- returnType name(type para1, type para2) {
+   //do something
+   return statement;
+   }*/
+
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class FunctionWithParameters {
+
+    public static int calculateSum(int num1,int num2) { // parameters or formal parameters
+        int sum = num1 + num2;
+        return sum; // sum in main & sum in calculatesum function are different variables
+    }
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter value of a : "); // Take input 1 from user
+        int a = sc.nextInt(); // store input 1 from user
+        System.out.print("Enter value of b : "); // Take inout 2 from user
+        int b = sc.nextInt(); // store input 2 from user
+        int sum = calculateSum(a, b); // arguments or actual parameters
+        System.out.println("The sum is " + sum);
+        sc.close(); // close scanner object
+    }
+}
---
+++ JavaBasics/Functions.java
@@ -0,0 +1,21 @@
+// Program to show working of functions
+/* Syntax -- returnType name() {
+ * // body
+ * return statement;
+ * } */
+
+package JavaBasics;
+
+public class Functions {
+    // public static is access modifier
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return; // not necessary only when return type is void
+    }
+    // on execution main function is executed 1st
+    public static void main(String args[]) {
+        printHelloWorld(); // function call
+    }
+}
